6f638d28375ff7bbd6cdd927c89cf0b2be02a4a5
Areli Celestin <147631872+MagnusVentus@users.noreply.github.com>
2025-05-04 07:52:26 -0500

    Add files via upload

---
+++ fs_functions.fs
@@ -0,0 +1,8 @@
+﻿module fs_functions
+
+let mathManipulation L =
+      match L with
+      | [] -> 0
+      | _  -> let resolution = List.map (fun x -> ((x*2) + 30)) L
+              List.reduce (fun x y -> x + y) resolution
+              
